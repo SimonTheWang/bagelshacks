90862fd147c684c0c13f2be3dcb66b05a86800f8
SimonTheWang <simonwg2001@gmail.com>
2025-02-24 20:55:29 -0500

    added more

--- src/Sponsors.tsx
+++ src/Sponsors.tsx
@@ -11,18 +11,26 @@ const Sponsors = ({className}: {className: string}) => {
                     <img src="/neo4j.png" alt="neo4j" className="w-[350px] m-auto p-auto" />
                 </div>
                 <div className="flex flex-wrap gap-4 justify-center">
-                    <div className="overflow-hidden flex justify-center items-center w-[400px] h-[400px]">
-                        <img src="/Rootly.png" alt="Rootly" className="m-5 p-4" />
-                    </div>
+                    <img src="/Panache.png" alt="Panache" className="w-[500px]" />
+                    <img src="/Rootly.png" alt="Rootly" className="w-[350px] m-auto p-auto" />
                 </div>
+
             </div>  
-            <h1 className="text-center text-xl font-bold">In colaboration with</h1>
+            <h1 className="text-center text-xl font-bold">In collaboration with</h1>
             <div className="flex flex-row gap-4">
-                <div className="w-1/2 overflow-hidden">
-                    <img src="/northstart.png" alt="nortstart" className="m-5 p-4" />
+                <div className="w-1/2 overflow-hidden flex justify-center items-center">
+                    <img src="/northstart.png" alt="nortstart" className="m-5 p-4 w-[1000px]" />
                 </div>
-                <div className="w-1/2 overflow-hidden">
-                    <img src="/elantech.png" alt="nortstart" className="m-5 p-4" />
+                <div className="w-1/2 overflow-hidden flex justify-center items-center">
+                    <img src="/elantech.png" alt="nortstart" className="m-5 p-4 w-[700px]" />
+                </div>
+            </div>
+            <div className="flex flex-row justify-center gap-4">
+                <div className="w-1/2 overflow-hidden flex justify-center items-center">
+                    <img src="/trails.png" alt="nortstart" className="w-[200px]" />
+                </div>
+                <div className="w-1/2 overflow-hidden flex justify-center items-center">
+                    <img src="/ensemble.png" alt="nortstart" className="w-[300px]" />
                 </div>
             </div>
         </div>
@@ -30,32 +38,39 @@ const Sponsors = ({className}: {className: string}) => {
         <div className={cn(className)}>
             <h2 className="text-center text-lg font-bold"> Exec Sponsors</h2>
             <div className="flex flex-col">
-                <div className="flex flex-row justify-center">
-                    <div className="flex flex-wrap justify-center">
-                        <img src="/Cohere.png" alt="Cohere" className="w-[300px]" />
-                    </div>
-                    <div className="flex flex-wrap justify-center">
-                        <img src="/neo4j.png" alt="neo4j" className="w-[150px] m-auto p-auto" />
-                    </div>
+                <div className="flex flex-row gap-4 justify-center items-center">
+                    <img src="/Cohere.png" alt="Cohere" className="w-[280px]" />
                 </div>
-
-                <div className="flex flex-wrap justify-center h-[0px]">
-
+                <div className="flex flex-row gap-4 justify-center items-center">
+                    <img src="/neo4j.png" alt="neo4j" className="w-[150px]" />
                 </div>
-
-                <div className="flex flex-wrap gap-8 justify-center mt-12">
-                    <div className="h-[50px] w-[150px] pt-5 overflow-hidden py-4 flex justify-center items-center">
-                        <img src="/Rootly.png" alt="Rootly" className="mt-10" />
-                    </div>
+                <div className="h-[40px]"></div>
+                <div className="flex flex-row gap-4 justify-center items-center mt-8">
+                    <img src="/Rootly.png" alt="Rootly" className="w-[220px]" />
+                </div>
+                <div className="h-[10px]"></div>
+                <div className="flex flex-row gap-4 justify-center items-center mt-8">
+                    <img src="/Panache.png" alt="Panache" className="w-[250px]" />
                 </div>
             </div>  
-            <h2 className="text-center text-xl font-bold mt-16">In colaboration with</h2>
-            <div className="flex flex-col gap-8">
-                <div className="overflow-hidden">
-                    <img src="/northstart.png" alt="nortstart" className="m-5 p-4" />
+            <div className="h-[30px]">
+
+            </div>
+            <h2 className="text-center text-xl font-bold mt-32">In colaboration with</h2>
+            <div className="flex flex-col gap-4">
+                <div className="flex flex-row gap-4 justify-center items-center">
+                    <img src="/northstart.png" alt="nortstart" className="m-5 p-4 w-[1100px]" />
                 </div>
-                <div className="overflow-hidden">
-                    <img src="/elantech.png" alt="nortstart" className="m-5 p-4" />
+                <div className="flex flex-row gap-4 justify-center items-center">
+                    <img src="/elantech.png" alt="nortstart" className="m-5 p-4 w-[700px]" />
+                </div>
+                <div className="h-[10px]"></div>
+                <div className="flex flex-row gap-4 justify-center items-center">
+                    <img src="/trails.png" alt="nortstart" className="w-[100px]" />
+                </div>
+                <div className="h-[10px]"> </div>
+                <div className="flex flex-row gap-4 justify-center items-center">
+                    <img src="/ensemble.png" alt="nortstart" className="w-[200px]" />
                 </div>
             </div>
         </div>
